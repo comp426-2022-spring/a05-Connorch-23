168729a337e6e38867c8c5e2cec56b4d534503b2
connorhitchings <connorch@live.unc.edu>
2022-04-21 10:02:23 -0400

    First main js commit

--- public/main.js
+++ public/main.js
@@ -1,8 +1,24 @@
 // Focus div based on nav button click
 
 // Flip one coin and show coin image to match result when button clicked
-
+// Button coin flip element
+const coin = document.getElementById("coin")
+// Add event listener for coin button
+			coin.addEventListener("click", flipCoin)
+			function flipCoin() {
+                fetch('http://localhost:5000/app/flip/')
+  				.then(function(response) {
+    			  return response.json();
+  				})
+				.then(function(result) {
+					console.log(result);
+					document.getElementById("result").innerHTML = result.flip;
+					document.getElementById("quarter").setAttribute("src", result.flip+".jpg");
+					coin.disabled = true
+				})
+            }
 // Flip multiple coins and show coin images in table as well as summary results
+
 // Enter number and press button to activate coin flip series
 
 // Guess a flip by clicking either heads or tails button
